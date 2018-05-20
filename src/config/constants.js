565d3ce9c85e664f699c35774f97f1fd3998a1e4
ishwarvirdi <30491372+ishwarvirdi@users.noreply.github.com>
2018-05-20 19:27:44 +1000

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 // Use 2nd one for local testing/environment. Do not commit with localhost.
-//const apiurl = "https://trunksmartreconcilespring.herokuapp.com";
-const apiurl = "http://localhost:8080";
+const apiurl = "https://trunksmartreconcilespring.herokuapp.com";
+//const apiurl = "http://localhost:8080";
 export {apiurl};
 
