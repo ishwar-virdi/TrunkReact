3664c51951aa0d7bb6aa8e6fe6c1f9b34917c984
ishwarvirdi <30491372+ishwarvirdi@users.noreply.github.com>
2018-05-20 13:56:28 +1000

    Updating receiptContent.js to current master
    To prevent wiping previous development work.

--- src/view/mainPage/result/receipt/receiptContent.js
+++ src/view/mainPage/result/receipt/receiptContent.js
@@ -3,6 +3,8 @@ import ReceiptTable from "./receiptTable";
 import { Link} from 'react-router-dom';
 import "../../../../stylesheets/mainPage/result/receipt/receiptContent.css";
 import React from "react";
+import axios from "axios/index";
+import {apiurl} from "../../../../config/constants";
 
 
 class ReceiptContent extends Component{
@@ -13,36 +15,73 @@ class ReceiptContent extends Component{
             receipt:props.receipt,
             transaction:{
                 header:["Transaction",""],
-                date:["Transaction Date","22/04/2018"],
-                time:["Transaction Time","15:20"],
-                reNumber:["Receipt Number","#32456675#"],
-                MID:["Merchant ID","TEST"],
-                TranSource:["Transaction Source",""]
+                date:["Transaction Date",""],
+                time:["Transaction Time",""],
+                customerName:["Customer Name",""],
+                MID:["Merchant ID",""],
+                bankReference:["Bank Reference",""],
             },
             status:{
-                header:["Status","Success"],
-                date:["Manually Marked","False"],
-            },
-            statement:{
-                header:["Statement",""],
-                date:["Statement Date","21/04/2018"],
+                header:["Status",""],
+                date:["Settlement Date",""],
+                isReconciled:["Reconciled",""],
+                reconcileDate:["Reconciled Date",""]
             },
             card:{
                 header:["Card",""],
-                date:["Card PAN","1234 5647 9879 2313"],
-                time:["Card Scheme","VISA"],
-                reNumber:["Currency","AUD"],
+                pan:["Card PAN",""],
+                scheme:["Card Scheme",""],
+                expiry:["Card Expiry",""],
             },
             amount:{
                 header:["Amount",""],
-                date:["Principle Amount","52"],
-                time:["Surcharge",""],
+                amount:["Principle Amount",""],
+                surcharge:["Surcharge",""],
+                currency:["Currency",""],
             },
         }
     };
 
 
     componentDidMount() {
+        axios.get(apiurl + "/receipt/" + this.state.receipt)
+            .then(
+                (response) => {
+                    this.setState({
+                        transaction:{
+                            header:["Transaction",""],
+                            date:["Transaction Date",response.data.TransactionDate],
+                            time:["Transaction Time",response.data.TransactionTime],
+                            customerName:["Customer Name",response.data.CustomerName],
+                            MID:["Merchant ID",response.data.MerchantID],
+                            bankReference:["Bank Reference",response.data.BankReference],
+                        },
+                        status:{
+                            header:["Status",response.data.Status],
+                            date:["Settlement Date",response.data.SettlementDate],
+                            isReconciled:["Reconciled",response.data.IfReconciled],
+                            reconcileDate:["Reconciled Date",response.data.ReconciledDate]
+                        },
+                        card:{
+                            header:["Card",""],
+                            pan:["Card PAN",response.data.CardPAN],
+                            scheme:["Card Scheme",response.data.CardScheme],
+                            expiry:["Card Expiry",response.data.CardExpiry],
+                        },
+                        amount:{
+                            header:["Amount",""],
+                            amount:["Principle Amount",response.data.PrincipleAmount],
+                            surcharge:["Surcharge",response.data.Surcharge],
+                            currency:["Currency",response.data.Currency],
+                        },
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        status: "ERROR",
+                    });
+                }
+            )
     }
 
     render(){
@@ -63,9 +102,6 @@ class ReceiptContent extends Component{
                         </div>
                     </div>
                     <div className="receipt-table">
-                        <div>
-                            <ReceiptTable data={this.state.statement}/>
-                        </div>
                         <div>
                             <ReceiptTable data={this.state.card}/>
                         </div>
