4eb9ad5b7c628cbcff61364258891a0753e28877
Oliver <oliver@rateitapp.com>
2018-05-27 14:21:05 +1000

    Added checkboxes back to table

--- src/view/mainPage/detail/transactionTable.js
+++ src/view/mainPage/detail/transactionTable.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "../../../stylesheets/mainPage/detail/transactionTable.css";
-import TransactionRow from './transactionRow';
+import NumberFormat from 'react-number-format';
 import {apiurl, selfurl} from "../../../config/constants";
 import axios from "axios/index";
-
-// React library
 import ReactTable from "react-table";
 
 class TransactionTable extends React.Component {
@@ -13,7 +11,8 @@ class TransactionTable extends React.Component {
 
         this.state = {
             items: [],
-            isAllChecked: false
+            selected: {},
+            selectAll: 0
         };
 
         this.jsonToResult = this.jsonToResult.bind(this);
@@ -40,147 +39,162 @@ class TransactionTable extends React.Component {
                 }
                 this.setState({
                     items: details,
-                    isAllChecked: false
+                    selectAll: 0
                 });
         });
-    }
+    };
 
     jsonToResult = (json) =>{
         let result = {};
-        result.uniqueId = json.id;
         result.isChecked = false;
         result.dateTime = json.date.slice(0, -12);
         result.description = json.description;
-        result.amount = json.amount;
-        result.accountNumber = json.accountNumber;
+        result.amount = <NumberFormat value={json.amount} displayType={'text'} thousandSeparator={true} prefix={'$'}/>;
+        result.receiptNumber = json.accountNumber;
         result.transactionType = json.transactionType;
-        result.reconciled = json.status;
+        if (json.status)
+            result.reconciled = "Success";
+        else
+            result.reconciled = "Failed";
         result.rule = json.rule;
         return result;
     };
 
+    markAsReconciled = () => {
+        console.log(this.state.selected);
 
-    // selectOne = (isChecked, index) => {
-    //     var items = this.state.items;
-    //
-    //     items[index].isChecked = !isChecked;
-    //
-    //     this.setState({
-    //         items: items
-    //     });
-    // };
-    //
-    // markAsReconciled = () => {
-    //     var items = this.state.items;
-    //
-    //     for (var i = 0; i < items.length; i++) {
-    //         if (items[i].isChecked) {
-    //             items[i].reconciled = true;
-    //             items[i].rule = "Manually Marked"
-    //         }
-    //     }
-    //
-    //     this.setState({
-    //        items: items
-    //     });
-    // };
-    //
-    // markAsNotReconciled = () => {
-    //     var items = this.state.items;
-    //
-    //     for (var i = 0; i < items.length; i++) {
-    //         if (items[i].isChecked) {
-    //             items[i].reconciled = false;
-    //             items[i].rule = "Manually Marked"
-    //         }
-    //     }
-    //
-    //     this.setState({
-    //         items: items
-    //     });
-    // };
-    //
-    // returnList = () => {
-    //     const items = this.state.items;
-    //     var list = [];
-    //
-    //     for (var i = 0; i < items.length; i++) {
-    //         list.push(<TransactionRow key={i} value={items[i]} isHeader={false} selectChanged={this.selectOne} index={i} backTo={this.props.id}/>);
-    //     }
-    //
-    //     return list;
-    // };
-    //
-    // selectAll = () => {
-    //     var items = this.state.items;
-    //     var changeTo = true;
-    //
-    //     if (this.state.isAllChecked)
-    //         changeTo = false;
-    //
-    //     for (var i = 0; i < items.length; i++) {
-    //         items[i].isChecked = changeTo;
-    //     }
-    //
-    //     this.setState({
-    //         items: items,
-    //         isAllChecked: changeTo
-    //     });
-    // };
+        axios({
+            withCredentials: true,
+            method: 'POST',
+            url: apiurl + "/api/markReconcile",
+            headers: {
+                'Content-Type' : 'application/json; charset=utf-8'
+            },
+            data: {
+                markAsReconcile: true,
+                items: this.state.selected
+            }
+        })
+            .then(
+                (response) => {
+
+                });
+        /*var items = this.state.items;
+
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].isChecked) {
+                items[i].reconciled = true;
+                items[i].rule = "Manually Marked"
+            }
+        }
+
+        this.setState({
+            items: items
+        });*/
+    };
+
+    markAsNotReconciled = () => {
+        console.log(this.state.selected);
+
+        axios({
+            withCredentials: false,
+            method: 'POST',
+            url: apiurl + "/api/markReconcile",
+            headers: {
+                'Content-Type' : 'application/json; charset=utf-8'
+            },
+            data: {
+                markAsReconcile: true,
+                items: this.state.selected
+            }
+        })
+            .then(
+                (response) => {
+
+                });
+
+/*        var items = this.state.items;
+
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].isChecked) {
+                items[i].reconciled = false;
+                items[i].rule = "Manually Marked"
+            }
+        }
+
+        this.setState({
+            items: items
+        });*/
+    };
+
+    toggleRow(receiptNumber) {
+        const newSelected = Object.assign({}, this.state.selected);
+        newSelected[receiptNumber] = !this.state.selected[receiptNumber];
+
+        this.setState({
+            selected: newSelected,
+            selectAll: 2
+        });
+    }
+
+    toggleSelectAll = () => {
+        let newSelected = {};
+
+        if (this.state.selectAll === 0) {
+            this.state.items.forEach(item => {
+                newSelected[item.receiptNumber] = true;
+            });
+        }
+
+        this.setState({
+            selected: newSelected,
+            selectAll: this.state.selectAll === 0 ? 1 : 0
+        });
+    };
 
     render () {
-
-        // table structure
+        // Table structure
         const { items } = this.state;
 
-
-        // var list = this.returnList();
-        // var isAllChecked = true;
-        //
-        // for (var i = 0; i < this.state.items.length; i++) {
-        //     if (this.state.items[i].isChecked === false) {
-        //         isAllChecked = false;
-        //         break;
-        //     }
-        // }
-        //
-        // const title  = {isChecked: isAllChecked,
-        //                 dateTime: "Transaction Date",
-        //                 description: "Customer",
-        //                 amount: "Amount",
-        //                 accountNumber: "Receipt Number",
-        //                 transactionType: "Transaction Type",
-        //                 reconciled: "Reconciled?",
-        //                 rule: "Reconciled Method"
-        // };
-
         return (
-
- /*           <div>
-                <table className="transaction-table">
-                    <thead>
-                    <TransactionRow value={title} isHeader={true} selectChanged={this.selectAll}/>
-                    </thead>
-                    <tbody>
-                    {list}
-                    </tbody>
-                </table>
-                <br />
-                <button type="button" onClick={this.markAsReconciled}>Mark as Reconciled</button>
-                <button type="button" onClick={this.markAsNotReconciled}>Mark as Failed</button>
-            </div>
-*/
             // Defining table structure
             <div>
                 <ReactTable
                     data={items}
                     columns={[
                         {
-                            Header: "Date / Time",
+                            id: "checkbox",
+                            accessor: "",
+                            Cell: ({ item }) => {
+                                return (
+                                    <input type="checkbox" className="checkbox"
+                                           checked={this.state.selected[item.receiptNumber] === true}
+                                           onChange={() => this.toggleRow(item.receiptNumber)}
+                                    />
+                                );
+                            },
+                            Header: x => {
+                                return (
+                                    <input type="checkbox" className="checkbox"
+                                        checked={this.state.selectAll === 1}
+                                        ref={input => {
+                                            if (input) {
+                                                input.indeterminate = this.state.selectAll === 2;
+                                            }
+                                        }}
+                                        onChange={() => this.toggleSelectAll()}
+                                    />
+                                );
+                            },
+                            sortable:false,
+                            width: 45
+                        },
+                        {
+                            Header: "Transaction Date",
                             accessor: "dateTime"
                         },
                         {
-                            Header: "Description",
+                            Header: "Customer Name",
                             accessor: "description"
                         },
                         {
@@ -188,8 +202,8 @@ class TransactionTable extends React.Component {
                             accessor: "amount"
                         },
                         {
-                            Header: "Account Number",
-                            accessor: "accountNumber"
+                            Header: "Receipt Number",
+                            accessor: "receiptNumber"
                         },
                         {
                             Header: "Transaction Type",
@@ -206,19 +220,19 @@ class TransactionTable extends React.Component {
                     ]
                     }
                     // Default page size
-                    defaultPageSize={10}
+                    defaultPageSize={20}
 
 
                     getTdProps={(state, rowInfo, column, instance) => {
                         return {
                             onClick: (e, handleOriginal) => {
+                                if (column.id === "checkbox")
+                                    return;
 
-                                console.log(rowInfo)
                                 if(rowInfo) {
-                                    let account = rowInfo['original']['accountNumber'];
-                                    // let backTo = rowInfo['original']['accountNumber'];
+                                    let account = rowInfo['original']['receiptNumber'];
 
-                                    let link = selfurl + "/transactiondetails/" + this.state.backTo + "/" + account
+                                    let link = selfurl + "/transactiondetails/" + this.props.id + "/" + account
 
                                     window.open(link,"_self");
                                 }
